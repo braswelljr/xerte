5184a90849b2cd9b05b1bd260e97758696acabfb
braswelljr <braswellkenneth7@gmail.com>
2023-06-16 18:24:35 +0000

    feat: authentication -> login

--- controller/v1/authentication/authentication.go
+++ controller/v1/authentication/authentication.go
@@ -1,13 +1,24 @@
 package authentication
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 // Login - Login is a function that handles the login of users.
 //
 //	@return fiber.Handler
 //	@return error
 func Login() fiber.Handler {
-	return func(ctx *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		// create a context with a timeout of 10 seconds for the request
+		_, cancel := context.WithTimeout(c.Context(), 10*time.Second)
+		defer cancel()
+
+		// get the basic auth credentials
+
 		return nil
 	}
 }
